a2a27130291b9410ad0bb7511a154957813a6aef
Nick Warren <nickwarren47@gmail.com>
2022-08-11 21:17:45 -0600

    commit tree ebe4bdd164b2ebdcc76c66de5eb5a9fd3ef2d406

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+require 'pry'
+
+class CashRegister 
+    attr_reader
+    attr_accessor :items, :discount
+    def initialize(items, discount)
+        @item= item
+        @discount= 0
+    end
+
+    # def total()
+end
+
 
